6b080d54e1bc4f059c6b98684468ccab0047377e
adriennk146 <aboboo146@gmail.com>
2020-09-29 11:29:10 +0200

    Osztály létrehozása metódusokkal.

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -17,8 +17,63 @@ namespace HarcosProjekt
 
         public Harcos(string nev, int statuszSablon)
         {
-            this.nev = nev;
-            this
+            
+        }
+
+        public string Nev
+        { 
+            get => this.nev; 
+            set => this.nev = value;
+        }
+        public int Szint 
+        { 
+            get => this.szint;
+            set => this.szint = value;
+        }
+        public int Tapasztalat 
+        {
+            get => this.tapasztalat;
+            set => this.tapasztalat = value; 
+        }
+        public int AlapEletero 
+        { 
+            get => this.alapEletero;
+        }
+        public int AlapSebzes
+        {
+            get => this.alapSebzes;
+        }
+        public int Eletero
+        {
+            get => this.eletero;
+            set => this.eletero = value;
+        }
+        public int Sebzes
+        {
+            get => this.alapSebzes + this.szint;
+        }
+        public int SzintLepeshez
+        {
+            get => 10 + this.szint * 5;
+        }
+        public int MaxEletero
+        {
+            get=> this.alapEletero + szint * 3;
+        }
+
+        public void Megkuzd(Harcos masikHarcos)
+        {
+
+        }
+
+        public void Gyogyul()
+        {
+
+        }
+
+        public string ToString()
+        {
+            return "";
         }
     }
 }
