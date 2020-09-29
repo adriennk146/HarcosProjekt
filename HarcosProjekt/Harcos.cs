656d5527dfe4fa0eb96447b3c94d478e277873d6
adriennk146 <aboboo146@gmail.com>
2020-09-29 11:13:05 +0200

    Add project files.

---
+++ HarcosProjekt/Harcos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarcosProjekt
+{
+    class Harcos
+    {
+        string nev;
+        int szint;
+        int tapasztalat;
+        int eletero;
+        int alapEletero;
+        int alapSebzes;
+
+        public Harcos(string nev, int statuszSablon)
+        {
+            this.nev = nev;
+            this
+        }
+    }
+}
