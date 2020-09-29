80b360d1b656d65341fef9f23865dfe306209faf
adriennk146 <aboboo146@gmail.com>
2020-09-29 13:06:31 +0200

    Harcosok kiíratása körök elején.

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -15,7 +15,7 @@ namespace HarcosProjekt
         static void Main(string[] args)
         {
             FelhasznaloHarcosa();
-          
+            Beolvasas();
             
            
 
@@ -52,6 +52,37 @@ namespace HarcosProjekt
             } while (statuszSablon < 1 || statuszSablon > 3);
             h1 = new Harcos(nev, statuszSablon);
         }
+
+
+        static void Beolvasas()
+        {
+            StreamReader sr = new StreamReader("harcosok1.csv");
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                string[] harcosAdatok = sor.Split(';');
+                Harcos h = new Harcos(harcosAdatok[0], int.Parse(harcosAdatok[1]));
+                harcosok.Add(h);
+            }
+        }
+
+        static void HarcosokKiiratasa()
+        {
+            int db = 1;
+
+            Console.WriteLine("\n\tA HARCOSOK:\n");
+            Console.WriteLine("#" + db + " -\t" + h1.ToString());
+            db++;
+            foreach (Harcos h in harcosok)
+            {
+                Console.WriteLine("#" + db + " -\t" + h.ToString());
+                db++;
+            }
+        }
+
+        
+
+
 
     }
 }
