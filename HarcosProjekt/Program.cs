8864162d4bfd0b247a4a36d16de73d2d05d42961
adriennk146 <aboboo146@gmail.com>
2020-09-29 13:04:54 +0200

    Felhasználó saját harcost hozhat létre.

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -9,23 +9,49 @@ namespace HarcosProjekt
 {
     class Program
     {
+        static List<Harcos> harcosok = new List<Harcos>();
+        static int fordulo = 0;
+        static Harcos h1;
         static void Main(string[] args)
         {
-            List<Harcos> harcosok = new List<Harcos>();
-            StreamReader sr = new StreamReader("harcosok1.csv");
-            while (!sr.EndOfStream)
-            {
-                string sor = sr.ReadLine();
-                string[] harcosAdatok = sor.Split(';');
-                Harcos h = new Harcos(harcosAdatok[0], int.Parse(harcosAdatok[1]));
-                harcosok.Add(h);
-            }
-            foreach(Harcos h in harcosok)
-            {
-                Console.WriteLine(h.ToString());
-            }
+            FelhasznaloHarcosa();
+          
+            
+           
 
             Console.ReadLine();
         }
+
+        static void FelhasznaloHarcosa()
+        {
+            Console.WriteLine("\t\t\tÜdv a játékban!\n" +
+                "Adj nevet a harcosodnak:");
+            string nev = Console.ReadLine();
+            int statuszSablon;
+            int ujra = 0;
+            do
+            {
+                if (ujra > 0)
+                {
+                    Console.WriteLine("Hibás értéket adtál meg!!");
+                }
+                Console.WriteLine("Milyen státusz sablonnal szeretnéd létrehozni a harcosodat?\n" +
+                "1 - Alap életerő: 15\tAlap sebzés: 3\n" +
+                "2 - Alap életerő: 12\tAlap sebzés: 4\n" +
+                "3 - Alap életerő:  8\tAlap sebzés: 5\n");
+                try
+                {
+                    statuszSablon = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    statuszSablon = 0;
+                }
+                
+                ujra++;
+            } while (statuszSablon < 1 || statuszSablon > 3);
+            h1 = new Harcos(nev, statuszSablon);
+        }
+
     }
 }
